b6e7c9aca633a342889816c36f40a5a545e8133e
saravananmsk@rediffmail.com <saravananmsk@rediffmail.com>
2020-08-11 07:14:37 +0530

    add files

---
+++ Python/python_win/ifelse03110820.py
@@ -0,0 +1,9 @@
+a = int(input("Enter the value between 1 to 10 \n"))
+if (a >= 0) and (a <= 3):
+    print("Work on to improve the customer rating")
+elif(a >= 4) and (a <= 7):
+    print("Customer rating is good")
+elif(a >= 8) and (a <= 10):
+    print("Customer rating is Excellent")
+else:
+    print("Please enter valid entry")
