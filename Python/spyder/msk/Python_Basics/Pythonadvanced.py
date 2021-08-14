755aebab2844ab88c01e10528c0c44b20d027e7a
saravananmsk@rediffmail.com <saravananmsk@rediffmail.com>
2021-08-14 20:25:10 +0530

    test

---
+++ Python/spyder/msk/Python_Basics/Pythonadvanced.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# File handling
+#Modules
+#Python Machine learning
+#Python Artifical inteligence
+#Python Database
+## Top Python Libraries
+    #Numpy
+    #Pandas
+    #Tensor flow
+    #Scikit-Learn
+    #Keras
+    #PyTorch
+    #lightGBM
+    #Eli5
+    #Scipy
+    #Theano
