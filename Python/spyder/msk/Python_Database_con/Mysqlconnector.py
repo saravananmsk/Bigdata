aae1959a11e3af39e2b51cfa6b4e3ac1de1f17ac
saravananmsk <saravananmsk01@gmail.com>
2022-02-18 13:21:25 +0530

    add files

--- Python/spyder/msk/Python_Database_con/Mysqlconnector.py
+++ Python/spyder/msk/Python_Database_con/Mysqlconnector.py
@@ -3,8 +3,9 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  passwd="mysqltest",
-  database="msk"
+  passwd="mysqldev",
+  database="msk",
+  auth_plugin='mysql_native_password'
 )
 
 mycursor = mydb.cursor()
